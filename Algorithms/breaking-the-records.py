47c7c7a0bd3ee01f77e33953e9efd08efd11a527
Akhilesh Upadhyaya <max.eclairs@gmail.com>
2021-09-13 21:39:10 +0530

    Update breaking-the-records.py

--- Algorithms/breaking-the-records.py
+++ Algorithms/breaking-the-records.py
@@ -13,7 +13,7 @@ import sys
 
 #
 # Complete the 'breakingRecords' function below.
-#
+# 
 # The function is expected to return an INTEGER_ARRAY.
 # The function accepts INTEGER_ARRAY scores as parameter.
 #
